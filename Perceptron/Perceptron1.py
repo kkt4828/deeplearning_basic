8081bd8c70105e102fb62ff022ea6ed2d1864c83
kkt <kkt4828@kakao.com>
2022-05-17 22:52:02 +0900

    편향 추가, NAND, OR 게이트 구현

--- Perceptron/Perceptron1.py
+++ Perceptron/Perceptron1.py
@@ -1,13 +1,45 @@
-def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
-    tmp = x1 * w1 + x2 * w2
+import numpy as np
 
-    if tmp > theta:
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.dot(x, w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+print('AND(0, 0)', AND(0, 0))
+print('AND(0, 1)', AND(0, 1))
+print('AND(1, 0)', AND(1, 0))
+print('AND(1, 1)', AND(1, 1))
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.dot(x, w) + b
+    if tmp > 0:
         return 1
     else:
         return 0
+print('NAND(0, 0)', NAND(0, 0))
+print('NAND(0, 1)', NAND(0, 1))
+print('NAND(1, 0)', NAND(1, 0))
+print('NAND(1, 1)', NAND(1, 1))
 
-print('AND(0, 0) : ', AND(0, 0))
-print('AND(1, 0) : ', AND(1, 0))
-print('AND(0, 1) : ', AND(0, 1))
-print('AND(1, 1) : ', AND(1, 1))
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.4
+
+    tmp = np.dot(x, w) + b
+    if tmp > 0:
+        return 1
+    else:
+        return 0
+print('OR(0, 0)', OR(0, 0))
+print('OR(0, 1)', OR(0, 1))
+print('OR(1, 0)', OR(1, 0))
+print('OR(1, 1)', OR(1, 1))
