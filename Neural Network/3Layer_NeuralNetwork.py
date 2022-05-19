7534cf96a58602c59b4dbed1e63e7533f02b5610
kkt <kkt4828@kakao.com>
2022-05-20 00:49:39 +0900

    Update - 3-Layer 신경망 간단한 구현 init_network 가중치 수정

--- Neural Network/3Layer_NeuralNetwork.py
+++ Neural Network/3Layer_NeuralNetwork.py
@@ -6,14 +6,15 @@ def Sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 def init_weight():
-    w1 = np.array([[1, 3, 5], [2, 4, 6]])
-    w2 = np.array([[3, 4], [5, 6], [7, 8]])
-    w3 = np.array([[-5, 3], [-2, 6]])
-    b1 = -5
-    b2 = -4
-    b3 = 3
+    network = {}
+    network['w1'] = np.array([[1, 3, 5], [2, 4, 6]])
+    network['w2'] = np.array([[3, 4], [5, 6], [7, 8]])
+    network['w3'] = np.array([[-5, 3], [-2, 6]])
+    network['b1'] = -5
+    network['b2'] = -4
+    network['b3'] = 3
 
-    return {'w1' : w1, 'w2': w2, 'w3' : w3, 'b1': b1, 'b2' : b2, 'b3' : b3}
+    return network
 
 def Layer3_NeuralNetwork(w_dict, x):
     a1 = np.dot(x, w_dict['w1']) + w_dict['b1']
